97d7816884dc4c560164486310acd51140bc9777
Ubuntu <ubuntu@ip-172-31-18-239.us-east-2.compute.internal>
2019-12-03 00:21:19 +0000

    Submitting Assessment 2

--- js-foundations.js
+++ js-foundations.js
@@ -2,24 +2,56 @@
 // Coding practical questions
 
 // 1. Write a function that takes a number as an argument and decides if the number is evenly divisble by three or not. Use the test cases 15, 0, 385, -7. Expected output: "15 is divisible by three", "0 is divisible by three", "385 is not divisble by three", "-7 is not divisble by three".
-
-
+const isDivisible = (num)=> {
+    if (num%3 === 0){
+        return `${num} is divisible by three.`
+    } else {
+        return `${num} is not divisible by three.`
+     }
+    }
+console.log(isDivisible(15))
+console.log(isDivisible(0))
+console.log(isDivisible(385))
+console.log(isDivisible(-7))
 
 // 2. Create an object called helloMe. Include your firstName, lastName and TWO OTHER properties of you. Add a method to your object that will return a sentence using the object properties to describe you.
 
+var helloMe = {
+    firstName: "Morganne", 
+    lastName: "Kayler",
+    city:  "Escondido", 
+    county: "San Diego",
+    getData: function() {
+        return this.firstName+" "+this.lastName+" lives in "+this.city+", "+this.county+"."
+    }
+}
 
+console.log(helloMe.getData())
 
 // ------------------------------------- Consider the variable:
 
 var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
 
-// 3a. Write a function that returns every other item from the array as one string. Expected output: "streetlamp teeth nephew database"
-
+// // 3a. Write a function that returns every other item from the array as one string. Expected output: "streetlamp teeth nephew database"
+const getEveryOther = (array) => {
+    let everyOther = array.filter((value, index)=>{
+        return index%2 ===0
+           })
+            return everyOther.join(" ")
+    }
+console.log(getEveryOther(randomNouns))
 
 
 // 3b. Create a function that takes in the variable and returns the array with all the words capitalized. Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
 
-
+function capitalLetter(array){
+ let arrayUppercase = []
+    for (let i=0; i < array.length; i++) {
+        arrayUppercase.push(array[i].charAt(0).toUpperCase() + array[i].substr(1));
+    }
+    return arrayUppercase;
+}
+console.log(capitalLetter(randomNouns))
 
 // 3c. STRETCH: Create a function that returns the length of each string from the randomNouns variable. Expected output: [10, 6, 5, 10, 6, 11, 8]
 
@@ -32,19 +64,32 @@ var testString2 = "academy"
 var testString3 = "sandiego"
 
 // 4a. Write a function called alphabetSoup that takes one argument and returns a string with all the letters in alphabetical order. Use each of the varibales as test cases. Expected output: "aelnr" "aacdemy" "adeginos"
-
-
+const alphabetSoup = (string) =>{
+    return string.split("").sort().join("")
+    }
+    console.log(alphabetSoup(testString1))
+    console.log(alphabetSoup(testString2))
+    console.log(alphabetSoup(testString3))
 
 // 4b. Write a function that takes in all three variables and returns a string with all the letters in alphabetical order. Expected output: "aaaacddeeegilmnnorsy"
 
-
+const grandmasAlphabetSoup=(...strings)=>{
+    return strings.join("").split("").sort().join("")
+    }
+    console.log(grandmasAlphabetSoup(testString1, testString2, testString3))
 
 // ------------------------------------- Consider the variables:
 var amounts = [9, 1, 8, 16, 5, 1, 42]
 var animals = ["ducks", "elephants", "pangolins", "zebras", "giraffes", "penguins", "llamas"]
 
 // 5a. Write a function that takes the two variables and returns a combined string. Expected output: "9 ducks 1 elephants 8 pangolins 16 zebras 5 giraffes 1 penguins 42 llamas"
-
+var animalNum = (array1, array2) => {
+    let combinedArrays = array1.map((value,index)=> {
+        return `${value} ${array2[index]}`
+    })
+    return combinedArrays.join("")
+}
+console.log (animalNum(amounts,animals))
 
 
 // 5b. STRETCH: Create a function that combines the two variables and updates the animal to be singular if the animal's corresponding number is 1. Expected output: "9 ducks 1 elephant 8 pangolins 16 zebras 5 giraffes 1 penguin 42 llamas"
--- js-foundations.md
+++ js-foundations.md
@@ -15,11 +15,12 @@ logIt()
 
 1a. Look at this Javascript function and try to predict what the console show.
 
-
+'outside' 'inside'
 
 1b. Test the function. Explain why the function returned what it did.
 
-  Your answer:
+  Your answer: 
+  doesn't accociate with console.log location
 
   Researched answer:
 
@@ -27,54 +28,72 @@ logIt()
 2. What is JSON?
 
   Your answer:
+JavaScript Object Notation
 
   Researched answer:
-
+JavaScript Object Notation.  Objects are Collections of key: value pairs
 
 3. What does CRUD stand for?
 
-  Your answer:
-
+  Your answer:  Don't know
+  
   Researched answer:
+  CRUD Application - 4 basic functions to manipulate data in a database:  
+  Create- make new records in application, 
+  Read - retreive/readout existing records in application,
+  Update - alter existing records in application
+  Delete - remove records in application
 
 
 
 4. What does are the 5 HTTP verbs?
 
-  Your answer:
+  Your answer: Don't know
 
-  Researched answer:
-
+  Researched answer: 
+  Request Methods - How a server should respond to a particular request: 
+  Get - read, simplest type of request
+  Post - create, usually filling out a form
+  Put - update/replace
+  Patch - update/modify
+  Delete
 
 5. What is a higher-order function?
 
-  Your answer:
+  Your answer:  There is more than one function involved in an operation.
 
-  Researched answer:
+  Researched answer:  A Higher Order Function is a function that receives a function as input argument or returns a function as output.
 
 
 6. STRETCH: What is a closure, what is it good for and how do you recognize one?
 
-  Your answer:
+  Your answer: Don't know
 
-  Researched answer:
+  Researched answer: Closure gives you access to an outer functions scope from an inner functions.
+  Combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment).
 
 
 7. STRETCH: What is an API?
 
-  Your answer:
-
-  Researched answer:
+  Your answer:  Don't know
+  
+  Researched answer:  
+  Application Programming Interface
+  Combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment).
+  API is the process of getting data from a website that is returned in JSON.
+  An API is a data to data interface, does not have any markup, just generates information.
 
 
 ### Additional Feedback Questions.
 
 1. Do you have a suggestion for a Check In question?
-
-
+ Pets
+ Favorite place in San Diego.
+ Best trip ever taken
 
 2. What was your favorite topic this week?
 
 
 
 3. What was your "A-ha!" moment this week?
+ It was getting more comfortable with working in Cloud 9 :) - until I got home and figured out there were still more situations where I didn't know how to redirect myself! 
